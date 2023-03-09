7f3e881d2a9ccedd68b759a8ba7abecefbccc2f7
Gabzon05 <nagygabi2002@gmail.com>
2023-03-09 11:49:59 +0100

    dsds

--- my.js
+++ my.js
@@ -12,17 +12,29 @@ createApp({
   data() {
     return {
       message: "Szia!",
-      todoCollection: [
+      todoCollection: [ 
         new Todo("reggeli"),
         new Todo("utazás"),
         new Todo("munka"),
         new Todo("mozi"),
-      ]
-    };
+      ],
+      editingTodoName: null,
+    }
   },
   methods: {
     nameView(completed){
       return completed ? "my-line-through my-green" : "my-red";
+    },
+    onEnterTodoName(todo){
+      if (!todo.name.trim()) {
+        todo.name = this.editingTodoName
+      }
+      todo.editing = false
+      
+    },
+    onDblClickTodoName(todo){
+      todo.editing = true;
+      this.editingTodoName = todo.name;
     }
   }
 }).mount("#app");
